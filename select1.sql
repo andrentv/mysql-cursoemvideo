6d0a18bd44742119f53351ad4698b28548834023
andrentv <comercial@vaccaros.com.br>
2021-07-23 21:54:06 -0300

    exerc

--- select1.sql
+++ select1.sql
@@ -9,6 +9,7 @@ select nome, descricao, ano, totaulas from cursos
 where totaulas between '20' and '60'
 order by ano, nome desc;
 
+#IN
 select nome, descricao, ano from cursos
 where ano in(2014, 2016)
 order by ano;
@@ -16,17 +17,40 @@ order by ano;
 select * from cursos
 where carga > 35 and totaulas < 30;
 
+#Like
 SELECT * from cursos
 where nome like 'P%';
 
 select * from cursos
-where nome like '%L_'; #like = parecido, semelhanças 'a%' inicio '%a' final '%a%' em qualquer lugar
-
+where nome like 'P_%P_'; #like = parecido, semelhanças 'a%' inicio '%a' final '%a%' em qualquer lugar
 
 update cursos set nome = 'JAVA' where idcurso = '5';
 
 insert into cursos values
-(default,'PHP7','Novo curso','50','25','2021');
+('9','PHP3','Curso antiogo','20','15','2012');
+
+UPDATE cursos SET `ano` = 2021 WHERE (`idcurso` = '8');
+
+SELECT `idcurso` FROM `cadastro`.`cursos`;
+
+#Distinct
+select distinct carga from cursos
+order by carga;
+
+#Agregação count()
+select count(nome) from cursos where carga < 40;
+
+select max(carga) from cursos 
+order by carga;
+
+select max(totaulas) from cursos where ano = '2016';
+
+select min(carga) from cursos where ano = '2016';
+
+select sum(totaulas) from cursos where ano = '2014';
+
+select avg(totaulas) from cursos where ano = '2014';
+
 
 
 DDL CREATE DATABASE
